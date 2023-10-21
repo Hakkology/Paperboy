36038332703f927b3ef03d71b62d8f1565a674f1
Hakkology <yildizhakan88@gmail.com>
2023-10-21 20:32:24 +0300

    Houses added.

---
+++ Assets/Scripts/Houses/HouseColors/house1.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class house1 : MonoBehaviour
+{
+    private string targetMaterialName = "map4";
+    private MeshRenderer meshRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRender = GetComponent<MeshRenderer>();
+
+        Material[] materials = meshRender.materials;
+
+        for (int i = 0; i < materials.Length; i++)
+        {
+            if (materials[i].name.StartsWith(targetMaterialName))
+            {
+                Color randomColor = new Color (Random.value, Random.value, Random.value);
+                materials[i].color = randomColor;
+                break;
+            }
+        }
+
+        meshRender.materials = materials;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
